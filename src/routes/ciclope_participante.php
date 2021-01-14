81d68fab47382b2f9b909ee6052ffc113a2c96bd
federicodonner <federico.donner@gmail.com>
2021-01-14 00:21:47 -0300

    feat(ciclope_participante): implementar update de participa

--- src/routes/ciclope_participante.php
+++ src/routes/ciclope_participante.php
@@ -34,3 +34,38 @@ $app->delete('/api/ciclope_participante/{id}', function (Request $request, Respo
         return messageResponse($response, $e->getMessage(), 500);
     }
 })->add($authenticate);
+
+// Registro de participantes para presentación
+$app->put('/api/ciclope_participante/{id}', function (Request $request, Response $response) {
+    try {
+        $id = $request->getAttribute('id');
+
+        // Verifica que el juego especificado exista
+        $sql = "SELECT * FROM ciclope_participante WHERE id = $id";
+        $db = new db();
+        $db = $db->connect();
+        $stmt = $db->query($sql);
+        $participantes = $stmt->fetchAll(PDO::FETCH_OBJ);
+
+        if ($participantes == null) {
+            $db = null;
+            return messageResponse($response, 'Participante no encontrado, verifica el identificador.', 404);
+        }
+
+        $participante = $participantes[0];
+        $participa = ($participante->participa == 1 ? 0 : 1);
+
+        // Si estoy acá es porque los campos del request están bien
+        $sql = "UPDATE ciclope_participante SET participa = $participa WHERE id = $id";
+        $stmt = $db->prepare($sql);
+        $stmt->execute();
+
+        $participante->participa = $participa;
+        // Devuelve el id de la actividad eliminada;
+        $db=null;
+        return dataResponse($response, $participante, 200);
+    } catch (PDOException $e) {
+        $db = null;
+        return messageResponse($response, $e->getMessage(), 500);
+    }
+})->add($authenticate);
